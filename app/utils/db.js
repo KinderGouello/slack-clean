bbdf7bc0f136ee157de0e2d9ac93c7c3a4627c32
Rémy Gouello <rigouello@gmail.com>
2018-03-13 23:26:27 +0100

    fix redis bug and refacto

--- app/utils/db.js
+++ app/utils/db.js
@@ -8,23 +8,21 @@ const getClient = () =>
     password: process.env.REDIS_PASSWORD || '',
   });
 
-const get = key => new Promise((resolve, reject) => {
-  getClient().get(key, (err, reply) => {
+const get = key =>
+  new Promise((resolve, reject) => getClient().get(key, (err, reply) => {
     if (err) reject(err);
 
     console.log(`Get key ${key} from Redis result :`, reply);
     resolve(reply);
-  });
-});
+  }));
 
-const set = (key, value) => new Promise((resolve, reject) => {
-  getClient().set((key, value), (err, reply) => {
+const set = (key, value) =>
+  new Promise((resolve, reject) => getClient().set(key, value, (err, reply) => {
     if (err) reject(err);
 
     console.log(`Save key ${key} to Redis with value ${value} and result :`, reply);
     resolve(reply);
-  });
-});
+  }));
 
 module.exports = {
   get,
