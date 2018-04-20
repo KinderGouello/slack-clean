8ba0dc10624501c2fc3c63905b0cb3840affefc3
Rémy Gouello <rigouello@gmail.com>
2018-04-20 10:25:53 +0200

    add slack workspace config

--- app/routes/activate.route.js
+++ app/routes/activate.route.js
@@ -4,7 +4,7 @@ const url = require('url');
 const clientId = process.env.SLACK_CLIENT_ID || '';
 const redirectUri = process.env.REDIRECT_URI || '';
 const state = process.env.STATE || '';
-const slackAuthUrl = 'https://slack.com/oauth/authorize';
+const slackAuthUrl = `https://${process.env.SLACK_WORKSPACE || ''}.slack.com/oauth/authorize`;
 const scope = 'files:read,files:write:user,users.profile:read';
 
 module.exports = (req, res) =>
--- app/utils/api.js
+++ app/utils/api.js
@@ -1,20 +1,16 @@
-const { promisify } = require('util');
 const slack = require('./slack-api-proxy');
 const moment = require('moment');
 
 const createClient = (token) => {
   const client = slack.createByUser(token);
-  const getFiles = promisify(client.files.list);
-  const getProfile = promisify(client.users.profile.get);
-  const deleteFile = promisify(client.files.delete);
 
   return {
-    getFiles: () => getFiles({
+    getFiles: () => client.files.list({
       ts_from: moment().subtract(1, 'months').format('X'),
       ts_to: moment().subtract(20, 'seconds').format('X'),
     }),
-    getProfile: user => getProfile({ user }),
-    deleteFile: id => deleteFile(id),
+    getProfile: user => client.users.profile.get({ user }),
+    deleteFile: id => client.files.delete(id),
   };
 };
 
@@ -25,7 +21,7 @@ const getAccessToken = ({
   redirectUri,
 }) => {
   const client = slack.createByApp();
-  const getToken = promisify(client.oauth.access);
+  const getToken = Promise.promisify(client.oauth.access);
 
   return getToken(clientId, clientSecret, code, redirectUri);
 };
