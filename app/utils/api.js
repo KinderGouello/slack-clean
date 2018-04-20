d777c51ebe10b33898dd252bf572cd37ea838878
Rémy Gouello <rigouello@gmail.com>
2018-04-20 11:29:10 +0200

    remove timestamp restriction

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,14 +1,10 @@
 const slack = require('./slack-api-proxy');
-const moment = require('moment');
 
 const createClient = (token) => {
   const client = slack.createByUser(token);
 
   return {
-    getFiles: () => client.files.list({
-      ts_from: moment().subtract(1, 'months').format('X'),
-      ts_to: moment().subtract(20, 'seconds').format('X'),
-    }),
+    getFiles: () => client.files.list(),
     getProfile: user => client.users.profile.get({ user }),
     deleteFile: id => client.files.delete(id),
   };
