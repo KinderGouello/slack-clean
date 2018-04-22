7d3544544da7120295526dfc2cad80efc40c00de
Rémy Gouello <rigouello@gmail.com>
2018-04-22 17:03:15 +0200

    promisify db and add del function

--- app/utils/db.js
+++ app/utils/db.js
@@ -1,28 +1,37 @@
 require('dotenv').config();
 const redis = require('redis');
+const { promisify } = require('util');
 
 const getClient = () =>
   redis.createClient({
     url: process.env.REDIS_URL,
   });
 
-const get = key =>
-  new Promise((resolve, reject) => getClient().get(key, (err, reply) => {
-    if (err) reject(err);
+const get = (key) => {
+  const client = getClient();
+  const getAsync = promisify(client.get).bind(client);
 
-    console.log(`Get key ${key} from Redis result :`, reply);
-    resolve(reply);
-  }));
+  return getAsync(key);
+};
 
-const set = (key, value) =>
-  new Promise((resolve, reject) => getClient().set(key, value, (err, reply) => {
-    if (err) reject(err);
+const set = (key, value) => {
+  const client = getClient();
+  const setAsync = promisify(client.set).bind(client);
 
-    console.log(`Save key ${key} to Redis with value ${value} and result :`, reply);
-    resolve(reply);
-  }));
+  return setAsync(key, value)
+    .then(res => console.log(`Save ${key} with value ${value} and result :`, res));
+};
+
+const del = (key) => {
+  const client = getClient();
+  const delAsync = promisify(client.del).bind(client);
+
+  return delAsync(key)
+    .then(res => console.log(`Delete ${key} and result :`, res));
+};
 
 module.exports = {
   get,
   set,
+  del,
 };
